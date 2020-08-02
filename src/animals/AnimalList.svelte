d6049f33fa32aded9fccf76c82f1522afd1db89a
Steve Brownlee <chortlehoort@gmail.com>
2020-08-02 08:55:41 -0500

    Comments

--- src/animals/AnimalList.svelte
+++ src/animals/AnimalList.svelte
@@ -2,14 +2,40 @@
   import AnimalProvider from "./AnimalProvider.svelte";
   import Animal from "./Animal.svelte";
 
+  /*
+    This is a component state variable that will hold all of
+    the animal objects that the data provider will extract from
+    the API. Initially set its value to an empty array.
+  */
   let animals = [];
-  let createAnimal;
 
-  const test_animal = { name: "Test", breed: "Jack Russell", customerId: 2, locationId: 1 };
+  /*
+    This state variable is to be a local reference to the createAnimal()
+    function that is defined in the AnimalProvider.
+  */
+  let saveAnimal;
+
+  /*
+    State variable holding an object representation of an animal.
+  */
+  const test_animal = {
+    name: "Test",
+    breed: "Jack Russell",
+    customerId: 2,
+    locationId: 1,
+  };
 </script>
 
+<!--
+    The `bind:createAnimal` directive allows this component to use a value exported
+    from a child component, but aliased as the variable scoped to the current
+    component.
+        "I need to invoke the createAnimal() function that is defined in
+            AnimalProvider, so I will bind (or alias) that function to the
+            `saveAnimal` variable I defined above."
+-->
 <AnimalProvider
-  bind:createAnimal={createAnimal}
+  bind:createAnimal={saveAnimal}
   on:animalStateChanged={(e) => (animals = e.detail.animals)} />
 
 <article class="animals">
@@ -20,7 +46,7 @@
 
 <button
   on:click={() => {
-    createAnimal(test_animal);
+    saveAnimal(test_animal);
   }}>
   Create Animal
 </button>
