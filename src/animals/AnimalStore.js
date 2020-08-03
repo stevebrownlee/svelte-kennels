d7d9af77d2913742b47feb66915fa90888f28e26
Steve Brownlee <chortlehoort@gmail.com>
2020-08-02 23:02:18 -0500

    Added simplistic sync mechanism

--- src/animals/AnimalStore.js
+++ src/animals/AnimalStore.js
@@ -2,15 +2,23 @@
 import { writable } from 'svelte/store';
 
 const { subscribe, set } = writable([]);  // Create store
+let _synced = false
 
 export const animals = { subscribe }  // Export subscriptable data
 
 export const getAnimals = async () => {  // Export GET call
-    const response = await fetch("http://localhost:8088/animals");  // Get API state
-    const animals = await response.json();
+    if (!_synced) {
+        // Set as synced immediately in case 2 components need data simultaneously
+        _synced = true
 
-    if (response.ok) {
-        set(animals);  // Set the data to API state
+        const response = await fetch("http://localhost:8088/animals");  // Get API state
+        const animals = await response.json();
+
+        if (response.ok) {
+            set(animals);  // States are in sync
+        } else {
+            _synced = false  // Request failed, states not in sync
+        }
     }
 };
 
@@ -25,7 +33,7 @@ export const createAnimal = async (animal) => {
     const new_animal = await response.json();
 
     if (response.ok) {
+        _synced = false
         await getAnimals()
     }
 }
-
