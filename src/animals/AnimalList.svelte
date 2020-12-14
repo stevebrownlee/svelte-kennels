e83b8e0dc3607d362f23034f427014cfcb49ad90
Steve Brownlee <chortlehoort@gmail.com>
2020-12-14 16:02:48 -0600

    Use the onMount lifecycle method

--- src/animals/AnimalList.svelte
+++ src/animals/AnimalList.svelte
@@ -1,48 +1,53 @@
 <script>
-  import { animals, getAnimals, createAnimal } from "./AnimalStore.js"; // Import the data store
+    import { animals, getAnimals, createAnimal } from "./AnimalStore.js";
+    import { onMount } from "svelte";
 
-  getAnimals(); // Kick off getting animals into application state
+    onMount(() => {
+        getAnimals();
+    });
 
-  const test_animal = {
-    name: "Test",
-    breed: "Jack Russell",
-    customerId: 2,
-    locationId: 1,
-  };
+    const test_animal = {
+        name: "Test",
+        breed: "Jack Russell",
+        customerId: 2,
+        locationId: 1,
+    };
 </script>
 
 <style>
-  .animalsContainer {
-    flex: 2;
-  }
-  .animal-list {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-  }
+    .container {
+        flex: 2;
+    }
 
-  .animal {
-    flex-basis: 30%;
-  }
+    .animals {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    .animal {
+        flex-basis: 30%;
+    }
 </style>
 
-<article class="animalsContainer">
-  <button on:click={
-      () => createAnimal(test_animal)
-    }>Create Test Animal</button>
-  <button on:click={() => {
-    getAnimals()
-    }}> Get Animals </button>
+<article class="container">
+    <button on:click={() => createAnimal(test_animal)}>Create Test Animal</button>
+    <button
+        on:click={() => {
+            getAnimals();
+        }}>
+        Get Animals
+    </button>
 
-  <div class="animal-list">
-    {#each $animals as animal}
-      <section class="animal">
-        <div class="animal--name">
-          <h2><a href="/animal/{animal.id}">{animal.name}</a></h2>
-        </div>
-        <div class="animal--breed">{animal.breed}</div>
-      </section>
-    {/each}
-  </div>
+    <div class="animals">
+        {#each $animals as animal}
+            <section class="animal">
+                <div class="animal--name">
+                    <h2><a href="/animal/{animal.id}">{animal.name}</a></h2>
+                </div>
+                <div class="animal--breed">{animal.breed}</div>
+            </section>
+        {/each}
+    </div>
 </article>
